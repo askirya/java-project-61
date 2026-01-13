d2df6a922402a9e1ef1f7a75506b8d902db365d4
Ippach <ippahossawi@gmail.com>
2026-01-13 19:58:48 +0300

    Этап 9. Игра: Простое ли число?

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,6 +5,7 @@ import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Gcd;
 import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 
 import java.util.Scanner;
 
@@ -18,11 +19,12 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         
         int choice = scanner.nextInt();
-        scanner.nextLine(); // consume newline
+        scanner.nextLine();
         
         switch (choice) {
             case 1 -> Greet.run();
@@ -30,6 +32,7 @@ public class App {
             case 3 -> Calc.run();
             case 4 -> Gcd.run();
             case 5 -> Progression.run();
+            case 6 -> Prime.run();
             case 0 -> System.out.println("See you later!");
             default -> System.out.println("Invalid choice. Try again.");
         }
---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,35 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import java.util.Random;
+
+public class Prime {
+    private static final String DESCRIPTION = 
+        "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    private static final int MAX_NUMBER = 100;
+    
+    private static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void run() {
+        String[][] questionsAnswers = new String[3][2];
+        Random random = new Random();
+        
+        for (int i = 0; i < 3; i++) {
+            int number = random.nextInt(MAX_NUMBER) + 1;
+            String answer = isPrime(number) ? "yes" : "no";
+            
+            questionsAnswers[i][0] = String.valueOf(number);
+            questionsAnswers[i][1] = answer;
+        }
+        
+        Engine.run(DESCRIPTION, questionsAnswers);
+    }
+}
